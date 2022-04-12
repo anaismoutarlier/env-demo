5d2daadefb34d0ca94af9c6323ebce40bddb102a
Anais <anaismoutarlier@gmail.com>
2022-04-12 12:03:00 +0200

    index.ejs | add nodeenv to display

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { img: process.env.IMG_URL });
+  res.render('index', { img: process.env.IMG_URL, node_env: process.env.NODE_ENV });
 });
 
 module.exports = router;
